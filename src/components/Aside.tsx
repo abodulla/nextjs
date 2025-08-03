b0787b0d76b4719839bf05f7a09be6decc1bb36f
abodulla <abdallahmoaz506@gmail.com>
2025-08-03 16:59:15 +0300

    feat: initialize Next.js project with TypeScript and Tailwind CSS
    - Add package.json with dependencies for React, Next.js, TypeScript, and Tailwind CSS.
    - Create postcss.config.mjs for Tailwind CSS integration.
    - Add favicon.ico for the application.
    - Create globals.css for global styles using Tailwind CSS.
    - Implement RootLayout component in layout.tsx for application structure.
    - Add Aside component for product catalog display.
    - Configure TypeScript with tsconfig.json for Next.js compatibility.

---
+++ src/components/Aside.tsx
@@ -0,0 +1,26 @@
+export default function Aside() {
+  return (
+    <>
+    <aside className="
+      flex-1
+      flex-col
+      max-w-[400px] 
+      min-h-[95vh]
+      bg-[#3f508d] 
+      border-l-2
+      mt-[15px] 
+      border-l-[#ccc] 
+       m-[3px] 
+       rounded-[15px]
+       md:text-left 
+       md:p-5 
+       md:w-auto 
+       w-full
+       p-[10px_0]
+    text-white" >
+      <h1 className="text-4xl font-bold p-[10px]">Products</h1>
+      <p className="mt-2 text-lg p-[50px]">Catalog</p>
+    </aside>
+    </>
+  );
+} 
